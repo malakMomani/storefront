6a02299a67ab30fb966d3e87bd04cd7e28bb328d
Malak Al-Momani <malak.momani.96@gmail.com>
2021-06-30 00:41:04 +0300

    still

--- storefront/src/store/actions.js
+++ storefront/src/store/actions.js
@@ -1,33 +1,55 @@
+import superagent from 'superagent';
+
+const api = 'https://api-server-0.herokuapp.com/products';
+
+export const getRemoteData = () => async (dispatch, state) => {
+  // 1- get the remote data with superagent
+  // 2- then dispatch an action with the response after we get it.
+  console.log("inside getRemoteData");
+  console.log(dispatch);
+  console.log(state)
+  return await superagent.get(api).then(res => {
+    dispatch(getAction(res.body));
+  });
+}
+
+export const getAction = payload => {
+  return {
+    type: 'GET',
+    payload: payload
+  }
+}
+
 
 export const active = (name) => {
-    return {
-      type: 'ACTIVE',
-      payload: name,
-    };
+  return {
+    type: 'ACTIVE',
+    payload: name,
   };
-  
-  export const increment = (product) => {
-    return {
-      type: 'INCREMENT',
-      payload: product,
-    };
+};
+
+export const increment = (product) => {
+  return {
+    type: 'INCREMENT',
+    payload: product,
   };
-  
-  export const show = (bool) => {
-    return {
-      type: 'SHOW_CART',
-      payload: bool,
-    };
+};
+
+export const show = (bool) => {
+  return {
+    type: 'SHOW_CART',
+    payload: bool,
   };
-  
-  export const decrement = (product) => {
-    return {
-      type: 'DECREMENT',
-      payload: product,
-    };
+};
+
+export const decrement = (product) => {
+  return {
+    type: 'DECREMENT',
+    payload: product,
   };
-  export const reset = () => {
-    return {
-      type: 'RESET'
-    }
+};
+export const reset = () => {
+  return {
+    type: 'RESET'
   }
+}
